658a7f44e8a9a21a0da5d8b5f0d9890a6360be45
MARISI BUNTHORA SIBARANI <127746137+marsib2002@users.noreply.github.com>
2023-03-28 13:57:39 +0700

    Add files via upload

---
+++ Aplikasi.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>PENJUMLAHAN</title>
+</head>
+
+<body>
+    <h5> APLIKASI PERHITUNGAN MATEMATIKA</h5>
+    <h5> MARISI BUNTHORA SIBARANI (221510017)</h5>
+    <form action="proses.php" method="post">
+       Bilangan 1 : <br>
+       <input type="text" name="txtbil1"><br>
+       Bilangan 2 : <br>
+       <input type="text" name="txtbil2"><br>
+       <button tyoe="submit">TAMBAH</button>
+    </form>
+</body>
+</html>
---
+++ proses.php
@@ -0,0 +1,10 @@
+<?php
+$no1 = $_POST['txtbil1'];
+$no2 = $_POST['txtbil2'];
+
+echo "Bilangan 1 :" .$no1. "<br>";
+echo "Bilangan 2 :" .$no2. "<br>";
+
+$tambah = $no1 + $no2 ;
+echo "hasil tambah = ".$tambah;
+?>
